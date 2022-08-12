a3945c97d309cfdf5fb494f638b3e448973daeec
Thaina Israel <thainaluizaisrael@gmail.com>
2022-08-12 14:20:42 -0300

    adivinha player

--- adivinhe_cpu.py
+++ adivinhe_cpu.py
@@ -20,3 +20,25 @@ def adivinhe(x):  # Definindo função. x = parametro
 
 
 adivinhe(10)
+
+
+import random
+
+
+def pc_adivinha(x):
+    menor = 1
+    maior = x
+    feedback = ''
+    while feedback != 'c':
+        if menor != maior:
+            adivinhe = random.randint(menor, maior)
+        else:
+            adivinhe = menor
+        feedback = input(
+            f'{adivinhe} é muito alto (A), muito baixo (B) ou correto (C)? ').lower()  # lower para que mesmo que o usuário digite em maiúsculo, o PC entenda em minusculo
+        if feedback == 'a':
+            maior = adivinhe - 1
+        elif feedback == 'l':
+            menor = adivinhe + 1
+        print(f'O CPU acertou o seu número! {adivinhe} era o correto!')
+    pc_adivinha(10)
